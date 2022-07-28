bd11aa944804a139456458d1570db55ada4beb80
Mike Stolley <stolleywilliam@yahoo.com>
2022-07-28 18:30:55 -0400

    added project section

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require("fs");
-// const generatePage = require("./src/page-template.js");
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 
 // const pageHTML = generatePage(name, github);
 
@@ -49,7 +49,14 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'about',
-            message: 'provide some information about yourself'
+            message: 'provide some information about yourself',
+            when: ({ confirmAbout }) => {
+                if (confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
     ])
 };
@@ -133,7 +140,13 @@ if (!portfolioData.projects) {
 
 
 promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-        console.log(portfolioData);
-    });
+  .then(promptProject)
+  .then(portfolioData => {
+     const pageHTML = generatePage(portfolioData);
+
+    // fs.writeFile('./index.html', pageHTML, err => {
+    //   if (err) throw new Error(err);
+
+    //   console.log('Page created! Check out index.html in this directory to see it!');
+    // });
+  });
